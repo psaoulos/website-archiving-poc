c23b5b757da3e0683d935bf208b18ec6f6325d97
Psaoylos <psaoulos@cosmote.gr>
2021-08-31 10:15:25 +0300

    Creating dummy env file in case of missing env vars

--- crawler/modules/Variables.py
+++ crawler/modules/Variables.py
@@ -40,7 +40,7 @@ def init_variables_from_env():
             MARIADB_IP = "mariadb"
         else:
             logger.error(f"No MARIADB_IP env var found, please specify in on root .env or consider running the docker-compose version.")
-            sys.exit(1)
+            provide_var_template_and_exit()
 
     if "MARIADB_USER" in os.environ:
         MARIADB_USER = os.getenv("MARIADB_USER").strip()
@@ -54,13 +54,13 @@ def init_variables_from_env():
             MARIADB_PASSWORD = os.getenv("MARIADB_ROOT_PASSWORD").strip()
         else:
             logger.error("No MARIADB_ROOT_PASSWORD env var found, please specify it on root .env.")
-            sys.exit(1)
+            provide_var_template_and_exit()
     else:
         if "MARIADB_PASSWORD" in os.environ:
             MARIADB_PASSWORD = os.getenv("MARIADB_PASSWORD").strip()
         else:
             logger.error("No MARIADB_PASSWORD env var found, please specify it on root .env.")
-            sys.exit(1)
+            provide_var_template_and_exit()
 
     if "MARIADB_PORT" in os.environ:
         MARIADB_PORT = int(os.getenv("MARIADB_PORT").strip())
@@ -71,4 +71,24 @@ def init_variables_from_env():
         MARIADB_DATABASE = os.getenv("MARIADB_DATABASE").strip()
     else:
         logger.error("No MARIADB_DATABASE env var found, please specify set it on root .env.")
-        sys.exit(1)
+        provide_var_template_and_exit()
+
+def provide_var_template_and_exit():
+    if not os.path.isfile("env"):
+        f = open("env", "a")
+        content = (
+            '# Please fill the following as instructed on the README.md file of project\n'
+            f'WEBPAGE_URL = "{"" if WEBPAGE_URL is None else str(WEBPAGE_URL)}"\n'
+            f'MARIADB_IP = "{"" if MARIADB_IP is None else str(MARIADB_IP)}"\n'
+            f'MARIADB_USER = "{"" if MARIADB_USER is None else str(MARIADB_USER)}"\n'
+            f'MARIADB_ROOT_PASSWORD = "{"" if MARIADB_ROOT_PASSWORD is None else str(MARIADB_ROOT_PASSWORD)}"\n'
+            f'MARIADB_PASSWORD = "{"" if MARIADB_PASSWORD is None else str(MARIADB_PASSWORD)}"\n'
+            f'MARIADB_PORT = "{"" if MARIADB_PORT is None else str(MARIADB_PORT)}"\n'
+            f'MARIADB_DATABASE = "{"" if MARIADB_DATABASE is None else str(MARIADB_DATABASE)}"\n'
+        )
+        f.write(content)
+        f.close()
+        logger.error("Dummy env file Created, Please fill it and copy it to project root folder as .env") 
+    else:
+        logger.error("Please fill env file and copy it to project root folder as .env")
+    sys.exit(1)
